1ef1c1dbda27a3ca75ffc2b98651cc2c7afe550b
Aaron VanSledright <aaronvansledright@Aarons-MacBook-Pro.local>
2019-06-03 11:50:16 -0400

    Removed uncessary stuff

--- deviceremove.py
+++ deviceremove.py
@@ -5,12 +5,9 @@ import pprint
 import json
 
 MONGO_HOST = raw_input("Enter IP of your Mongo DB: ")
-MONGO_DB = raw_input("Enter the name of your DB: ")
 device_mac = raw_input("Enter MAC Address - lowercase: ")
-#device_mac = raw_input("Enter your MAC Address: ")
-#MONGO_PORT = raw_input("Enter your port: ")
 
-client = pymongo.MongoClient('127.0.0.1', 27117)
+client = pymongo.MongoClient(MONGO_HOST, 27117)
 
 db = client['ace']
 results = (db.device.remove({ 'mac' : device_mac }))
